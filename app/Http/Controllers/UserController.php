e740f89a649cbadc728f62a5b7768c927194d514
FarisHakim <farisabdielhakim@gmail.com>
2023-01-28 00:00:18 +0700

    get data api country & age and insert to db

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,4 +16,41 @@ class UserController extends Controller
         $name=User::pluck('firstname','id');
         return $name;
     }
+    public function getDataApi($url){
+        $path='';
+        $name=$this->getAllName();
+        foreach ($name as $list) {
+            $path=$path.'name[]='.$list.'&';
+        }
+        $url.=$path;
+        $response=json_decode(HTTP::get($url));
+        return $response;
+    }
+    public function getAge(){
+        $user=$this->getAllName();
+        $id=array_keys($user->toArray());
+        $url='https://api.agify.io/?';
+        $ages=array();
+        $data=$this->getDataApi($url);
+        for ($i=0; $i <count($data) ; $i++) { 
+            $ages['id']=Str::uuid()->toString();;
+            $ages['id_user']=$id[$i];
+            $ages['count']=$data[$i]->count;
+            $ages['age']=$data[$i]->age;
+            Age::insert([$ages]);
+        }
+    }
+    public function getNationality(){
+        $nationalitys=array();
+        $user=$this->getAllName();
+        $id=array_keys($user->toArray());
+        $url='https://api.nationalize.io/?';
+        $data=$this->getDataApi($url);
+        for ($i=0; $i <count($data) ; $i++) { 
+            $nationalitys['id']=Str::uuid()->toString();;
+            $nationalitys['id_user']=$id[$i];
+            $nationalitys['id_country']=$data[$i]->country[0]->country_id;
+            Nationality::insert([$nationalitys]);
+        }
+    }
 }
